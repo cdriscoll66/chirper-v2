82647b7cc26a038fc5f72de32aea10be4969e736
Conor Driscoll <cdriscoll66@gmail.com>
2022-12-12 13:03:32 -0500

    creating chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChirpController extends Controller
 {
@@ -15,6 +16,8 @@ class ChirpController extends Controller
     public function index()
     {
         //
+        return Inertia::render('Chirps/Index', [
+        ]);
     }
 
     /**
@@ -36,6 +39,13 @@ class ChirpController extends Controller
     public function store(Request $request)
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
